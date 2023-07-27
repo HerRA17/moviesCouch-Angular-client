13da0ccad22fd435c923ff83855953783f6fc42a
Hermann Rasch <hermann.rasch93@gmail.com>
2023-07-27 16:48:12 +0200

    Addition of registration & login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { RetrivingServiceAPIDataService } from './retrieve-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'moviesCouch-Angular-client';
+
+  constructor(public dialog: MatDialog) {}
+  // function opens dialog when sign up button is clicked
+  openUserRegistration(): void{
+      this.dialog.open(UserRegistrationFormComponent, {
+        width: '290px'
+      });
+  }
+  // function opens dialog when login button is clicked
+  openUserLogin(): void{
+    this.dialog.open(UserLoginFormComponent, {
+      width: '290px'
+    })
+  }
 }
