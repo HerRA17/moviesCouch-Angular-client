d667cd24842e0a49700caa794523dc0f474d849b
Hermann Rasch <hermann.rasch93@gmail.com>
2023-07-27 16:51:12 +0200

    Addition of user-login logic

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+// This import is used to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// This import brings in the API calls 
+import { RetrivingServiceAPIDataService } from '../retrieve-api-data.service';
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() loginData = {Username: '', Password: ''};
+  constructor(
+    public retrievingServiceApiData: RetrivingServiceAPIDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar ) {  } 
+    ngOnInit(): void{ 
+    }
+    // function responsible for sending the form inputs to the backend
+    loginUser(): void {
+      this.retrievingServiceApiData.userLogin(this.loginData).subscribe((response) => {
+        localStorage.setItem('Username', response.user.Username);
+        localStorage.setItem('token', response.token);
+        // logic for successful login
+        this.dialogRef.close() //close dialog on success
+        console.log(response);
+        this.snackBar.open('Welcome back', 'Ok', {
+          duration: 2000
+        });
+      }, (response) => {
+          console.log(response);
+          this.snackBar.open(response, 'OK', {
+          duration: 2000
+      })
+      });
+    }
+}
