fa2e14989216207658c08c8b1073fdf1efd8d46e
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-04 17:21:08 +0200

    Update to navbar component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,22 +7,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  @Input() user: any;
+   
   constructor(
-    private router: Router
+    private router: Router,
+    private  renderer: Renderer2
   ) {}
   ngOnInit(): void {
-    }
+    // span identifier fro visual hidden
+    const spanElement = document.querySelector('.icon-reader');
+    // adding cdk-visually-hidden class
+    this.renderer.addClass(spanElement, 'cdk-visually-hidden');  
+  }
 
     toMovies(): void{
-      this.router.navigate(['Movies']);
+      this.router.navigate(['/Movies']);
     }
 
     toProfile(): void{
-      this.router.navigate(['Profile']);
+      this.router.navigate(['/Profile']);
     }
 
     logOut(): void{
-      this.router.navigate(['Welcome']);
+      this.router.navigate(['/Welcome']);
       localStorage.clear();
     }
 }
