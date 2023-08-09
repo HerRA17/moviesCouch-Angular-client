8f26ff2fb8c0fd3be734a35c7789fea0b0d64dba
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-09 13:27:28 +0200

    Rethought logic of functions to correct bug

--- src/app/user-profile-view/user-profile-view.component.ts
+++ src/app/user-profile-view/user-profile-view.component.ts
@@ -17,110 +17,60 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./user-profile-view.component.scss']
 })
 export class UserProfileViewComponent implements OnInit{
-user: any = { };
-favoriteMovies: any = [];
+user: any;
+favMovies: any = [];
 
 @Input() userData = {Username: '', Password: '', Email: '', Birthdate: ''}
 constructor(
     public retrievingServiceApiData: RetrivingServiceAPIDataService,
     public snackBar: MatSnackBar,
-    // public dialogRef: MatDialogRef,
     private router: Router
 ) {}
 
 ngOnInit(): void{
-  this.getUserInfo();
-  this.getFavoriteMovies();
-
-
-  // Subscribe to favoriteMovies updates
-  this.retrievingServiceApiData.updateUserObject.subscribe(() => {
-    this.getUserInfo();
-    this.getFavoriteMovies();
-  })
+  this.getUserInfo(); // error -ngOnInit
 }
 
-getUserInfo(): string | undefined {
-    const userString = localStorage.getItem('user');
-    if (userString) {
-      this.user = JSON.parse(userString);
-      return this.user; 
-    } else { 
-      this.router.navigate(['welcome']);
-      return
-    }
+getUserInfo(): any {
+    this.retrievingServiceApiData.getUser().subscribe((user) =>{
+      console.log(user);
+      this.user = user;
+      this.userData.Username = this.user.Username;
+      this.userData.Email = this.user.Email;
+      this.userData.Birthdate = this.user.Birthdate;
+      this.retrievingServiceApiData.getAllMovies().subscribe((response: any) => {
+        this.favMovies = response.filter((Movie: any)=> this.user.favMovies.includes(Movie._id)) // error
+      return this.user;
+      });
+    });
   }
 
-getUpdatedUserDetails(): any {
-  let updateUserData: {
-    Username?: string;
-    Password?: string;
-    Email?: string;
-    Birthdate?: string;
-  } = {};
-  
-  if (this.userData.Username !== '') {
-    updateUserData.Username = this.userData.Username;
-  } if (this.userData.Password !== '') {
-    updateUserData.Password = this.userData.Password;
-  } if (this.userData.Email !== '') {
-    updateUserData.Email = this.userData.Email;
-  } if (this.userData.Birthdate !== '') {
-    updateUserData.Birthdate = this.userData.Birthdate;
-  }
-  return Object.keys(updateUserData).length === 0 ? null : updateUserData;
-}
 
 updateUser(): void{
-  const updateUserData = this.getUpdatedUserDetails();
-  if (!updateUserData) {
-    this.snackBar.open('No changes to update!', 'OK', {
+  this.retrievingServiceApiData.editeUser(this.userData).subscribe((result) => {
+    console.log(result);
+    localStorage.setItem('user', JSON.stringify(result));
+    this.snackBar.open('User successfully updated', 'OK', {
+      duration: 2000
+    })
+  }, (result) => {
+    this.snackBar.open(result, 'OK', {
       duration: 2000
     });
-    return;
-  }
-  this.retrievingServiceApiData.editeUser(updateUserData).subscribe({
-    next: (result) => {
-      console.log(result);
-      // this.dialogRef.close();
-      this.snackBar.open('Update successfull!', 'OK', {
-        duration: 2000
-      });
-      localStorage.setItem('user', JSON.stringify(result));
-      this.retrievingServiceApiData.updateUserObject.next();
-    },
-    error: (error) => {
-      let errorMessage = error.Message;
-      console.error(errorMessage);
-      this.snackBar.open(errorMessage, 'OK', {
-        duration: 2000
-      });
-    }
-  });
-}
-
-getFavoriteMovies() {
-  const usersFavoriteMovies =  this.user.favoriteMovies;
-  this.retrievingServiceApiData.getAllMovies().subscribe((resp) => {
-    console.log('getFavoriteMovies response: ' + resp);
-    this.favoriteMovies = usersFavoriteMovies.map((MovieID: string) => {
-      return resp.find((Movie: any) => Movie._id === MovieID)
-    });
-    console.log('this are the favorite movies: ' + this.favoriteMovies)
   });
 }
 
 
 deleteAccount(): void {
 this.retrievingServiceApiData.deleteUser().subscribe((result) => {
-  console.log(result);
   localStorage.clear();
-  this.router.navigate(['welcome']);
-  this.snackBar.open('User successfully deleted', 'OK', {
+  console.log(result);
+  this.router.navigate(['Welcome']);
+  this.snackBar.open(result.message, 'OK', {
     duration: 2000
   });
 }, (result) => {
-  this.snackBar.open(result , 'OK' , {
+  this.snackBar.open(result.message , 'OK' , {
     duration: 2000
   })
 })
