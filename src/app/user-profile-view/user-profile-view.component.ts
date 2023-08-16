f13a3736c20b305185b2aa2b2316fcab7d2d5689
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-16 21:23:49 +0200

    Addition of comments (typedoc)

--- src/app/user-profile-view/user-profile-view.component.ts
+++ src/app/user-profile-view/user-profile-view.component.ts
@@ -10,12 +10,13 @@ import { Router } from '@angular/router';
 // date formating
 import { DatePipe } from '@angular/common';
 
-
+/** Component for displaying and managing user profile information */
 @Component({
   selector: 'app-user-profile-view',
   templateUrl: './user-profile-view.component.html',
   styleUrls: ['./user-profile-view.component.scss']
 })
+
 export class UserProfileViewComponent implements OnInit{
 user: any;
 favoriteMovies: any = [];
@@ -30,29 +31,28 @@ constructor(
 ngOnInit(): void{
   this.getUserInfo();
 }
-
+/** Makes call of the get user function
+ * @returns user object, extracted data from th euser object (Username, Email, Birthdate), and the array containing favorite movies
+ */
 getUserInfo(): any {
     this.retrievingServiceApiData.getUser().subscribe((user) =>{
-      console.log('user', user);
       this.user = user;
       this.userData.Username = this.user.Username;
       this.userData.Email = this.user.Email;
       this.userData.Birthdate = this.user.Birthdate;
       this.retrievingServiceApiData.getAllMovies().subscribe((response: any) => {
-        console.log('Response: ', response)
-        console.log('User favorite movies' , this.user.favoriteMovies)
         this.favoriteMovies = response.filter((movie: any)=> this.user.FavoriteMovies.includes(movie._id))
-        console.log('Favorite movies after filtering', this.favoriteMovies);
-        console.log('User after filtering', this.user);
-      return this.user;
+        return this.user;
       });
     });
   }
 
-
+/** Makes call of the edit user function
+ * @params user object
+ * @returns user object with the edited data
+*/
 updateUser(): void{
   this.retrievingServiceApiData.editeUser(this.userData).subscribe((result) => {
-    console.log(result);
     localStorage.setItem('user', JSON.stringify(result));
     this.snackBar.open('User successfully updated', 'OK', {
       duration: 2000
@@ -64,11 +64,11 @@ updateUser(): void{
   });
 }
 
-
+/** Makes call of the delete user function and redirects to the Welcome page
+ */
 deleteAccount(): void {
 this.retrievingServiceApiData.deleteUser().subscribe((result) => {
   localStorage.clear();
-  console.log(result);
   this.router.navigate(['Welcome']);
   this.snackBar.open(result.message, 'OK', {
     duration: 2000
