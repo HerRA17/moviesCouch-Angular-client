4b86a8e086957e8301417b3c8e8039e4b325e240
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-16 21:24:15 +0200

    Addition of comments (typedoc)

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -6,8 +6,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
+/** Component displaying information about movie details */
 export class MovieDetailsComponent {
 constructor(
+  /** Injects data about movie details into the component.
+   * @params The data object containing genre information: Titlee & Description 
+   */
   @Inject(MAT_DIALOG_DATA)
   public data: {
     Title: string,
