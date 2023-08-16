887222292fd5630ccb2e7be6cab0d4c5c6f4bd10
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-16 21:24:20 +0200

    Addition of comments (typedoc)

--- src/app/genre/genre.component.ts
+++ src/app/genre/genre.component.ts
@@ -6,8 +6,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './genre.component.html',
   styleUrls: ['./genre.component.scss']
 })
+/** Component displaying information about genre */
 export class GenreComponent {
 constructor(
+  /** Injects data about the genre into the component.
+   * @params The data object containing genre information: Name & Description 
+   */
   @Inject(MAT_DIALOG_DATA)
   public data: {
     Name: string,
