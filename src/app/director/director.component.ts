7a39dc71b05e82245541d97570de7ce8d77ce6bc
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-16 21:24:26 +0200

    Addition of comments (typedoc)

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -6,8 +6,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './director.component.html',
   styleUrls: ['./director.component.scss']
 })
+/** Component displaying  information about director */
 export class DirectorComponent {
 constructor(
+  /** Injects data about the director into the component.
+   * @params The data object containing genre information: Name, Bio, Birthdate & Deathdate 
+   */
   @Inject(MAT_DIALOG_DATA)
     public data: {
       Bio: string,
